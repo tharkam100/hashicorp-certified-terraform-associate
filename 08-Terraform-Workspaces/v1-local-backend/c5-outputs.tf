38c34f2f4bd98ff922354844083cb836a13e847c
tharkam100 <72263813+tharkam100@users.noreply.github.com>
2021-08-03 18:03:49 +0100

    Update c5-outputs.tf

--- 08-Terraform-Workspaces/v1-local-backend/c5-outputs.tf
+++ 08-Terraform-Workspaces/v1-local-backend/c5-outputs.tf
@@ -12,3 +12,5 @@ output "ec2_publicdns" {
   description = "Public DNS URL of an EC2 Instance"
   value = aws_instance.my-ec2-vm.*.public_dns
 }
+# The * in value is used when there is more than 1 instance and the output for this 
+# needs to be displaed as a list
